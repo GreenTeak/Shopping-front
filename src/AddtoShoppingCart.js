eabc056c86580175e740d8c418dd74f2c0fd2bd5
greenteak <greenteak1992@gmail.com>
2018-02-21 03:23:41 +0800

    addtoshopcart setstate

--- src/AddtoShoppingCart.js
+++ src/AddtoShoppingCart.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import './AddtoShoppingCart.css'
 
 class AddtoShoppingCart extends Component {
-    handleClickOnAddtoShop (e) {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
 
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
     render(){
         return (
             <div className="ShoppingCart">
-                购买数量：<input type="text" ref="buynum"></input>
-                <button >加入购物车</button>
+                <form >
+                    购买数量：<input type="text" ref="buynum" value={this.state.value} onChange={this.handleChange} ></input>
+                    <button onClick={this.handleSubmit}>加入购物车</button>
+                </form>
             </div>
         );
     }
---
+++ src/Itemview.js
@@ -0,0 +1,60 @@
+import React,{Component}from 'react'
+import ReactDOM from 'react-dom'
+import './Itemview.css'
+import AddtoShoppingCart from "./AddtoShoppingCart";
+const items=[
+    {
+        barcode: 'ITEM000000',
+        name: '可口可乐',
+        unit: '瓶',
+        price: 3.00
+    },
+    {
+        barcode: 'ITEM000001',
+        name: '雪碧',
+        unit: '瓶',
+        price: 3.00
+    },
+    {
+        barcode: 'ITEM000002',
+        name: '苹果',
+        unit: '斤',
+        price: 5.50
+    },
+    {
+        barcode: 'ITEM000003',
+        name: '荔枝',
+        unit: '斤',
+        price: 15.00
+    },
+    {
+        barcode: 'ITEM000004',
+        name: '电池',
+        unit: '个',
+        price: 2.00
+    },
+    {
+        barcode: 'ITEM000005',
+        name: '方便面',
+        unit: '袋',
+        price: 4.50
+    }
+]
+class Itemview extends Component{
+    render() {
+        const ItemElement = []
+        for (let item of items) {
+            ItemElement.push(
+                <div className="shoppingitem" >
+                    <div>{item.name}</div>
+                    <div>{item.price}元/{item.unit}</div>
+                    <AddtoShoppingCart />
+                </div>
+            )
+        }
+        return (
+            <div>{ItemElement}</div>
+        )
+    }
+}
+export default Itemview;
